c27de5fb69c2cdf14694b73c2c331fe421ed8016
sushma <kesamsettysushma@gmail.com>
2021-12-30 14:46:17 -0800

    leaflet part 1 - No legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,7 @@
 // URL to fetch the United States Geological Survey - Earthquake data for the past 7 days. 
 var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
-
-console.log("Hello there!")
+// var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+// var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
 
 // Perform GET request to the query URL
 d3.json(queryURL).then((data) => {
@@ -45,12 +44,50 @@ d3.json(queryURL).then((data) => {
         accessToken: API_KEY
     });
 
+    // Function to pick marker color based on the depth of the earthquake
+    function markerColorByDepth(depth) {
+        if(depth > 90) {
+            return "orangered";
+        } else if (depth > 70) {
+            return "darkorange";
+        } else if (depth > 50) {
+            return "orange";
+        } else if (depth > 30) {
+            return "gold";
+        } else if (depth > 10) {
+            return "greenyellow";
+        } else {
+            return "lawngreen";
+        }
+    }
+
+    // Function to set the marker size based on earthquake's magnitude
+    function markerSizeByMagnitude(magnitude) {
+            // console.log(`Magnitude: ${magnitude}; Radius: ${magnitude * 5}`)
+            return magnitude * 5;
+    }
+
     // Create a GeoJSON Layer containing the features array on the earthquake object
     // Run the onEachFeature function once for each piece of data in the array
     var earthquakeLayer = L.geoJSON(earthquakeData, {
-        onEachFeature: (feature, Layer) => Layer.bindPopup(`<h3>${feature.properties.place}</h3><h4>Magniture: ${feature.properties.mag}</h4>
-                        <hr><p>${new Date(feature.properties.time)}</p>`)
+        pointToLayer: function(feature, latlng) {
+                 return L.circleMarker(latlng);
+         },
+        style: function(feature) {
+            return {
+              color: "white",
+              fillColor: markerColorByDepth(feature.geometry.coordinates[2]),
+              fillOpacity: 0.5,
+              weight: 1.5,
+              radius: markerSizeByMagnitude(feature.properties.mag)
+            };
+          },
+        onEachFeature: (feature, Layer) => Layer.bindPopup(`<h3>${feature.properties.place}</h3><br>
+                                                            Magniture: ${feature.properties.mag}<br>
+                                                            Depth: ${feature.geometry.coordinates[2]}<br>
+                                                            <hr><p>${new Date(feature.properties.time)}</p>`)
     });
+
 
     // Define a baseMaps object to hold our base layers 
     var baseMaps = {
@@ -68,10 +105,33 @@ d3.json(queryURL).then((data) => {
     // Create our map, giving it the satellite map and earthquake layers to display on load
     var myMap = L.map("map", {
         // center: [34.052235, -118.243683],
-        center: mapCenter,
+        // center: mapCenter,
+        center: [31.7917, 7.0926],
         zoom: 3,
-        layers: [satelliteMap, earthquakeLayer]
+        layers: [grayscaleMap, earthquakeLayer]
     });
+
+    // Create Legend Control
+    var labels = ['-10-10', '10-30', '30-50', '50-70', '70-90', '90+'];
+    var colors = ['orangered', 'darkorange', 'orange', 'gold', 'greenyellow', 'lawngreen'];
+
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend');
+        
+        for (var i = 0; i < labels.length; i++) {
+        
+                div.innerHTML += 
+                   '<i style="background:' + colors[i] + '"></i> ' + labels[i] + '<br>';
+            }
+        console.log(div);
+        console.log(div.innerHTML);
+
+        return div;
+    }; 
+    legend.addTo(myMap);
 
     // Create a Layer Control
     // Pass in our baseMaps and overlayMaps
@@ -79,5 +139,6 @@ d3.json(queryURL).then((data) => {
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap) 
+
 
 });
